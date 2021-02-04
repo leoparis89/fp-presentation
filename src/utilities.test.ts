ff47e4282ff5b45f8d395532aa5dbe218eb3d66e
Lev Kowalski <levkowalski@gmail.com>
2021-02-04 23:07:45 +0100

    wip

--- src/utilities.test.ts
+++ src/utilities.test.ts
@@ -6,6 +6,8 @@ describe("curry", () => {
     const add = curry(_add);
     const add2 = add(2);
 
+    expect(add(2, 2)).toEqual(4);
+    expect(add(2)(2)).toEqual(4);
     expect(add2(2)).toEqual(4);
   });
 });
