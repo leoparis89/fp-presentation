09c42b41f2a5a82652890ac5572bb84263208878
Lev Kowalski <levkowalski@gmail.com>
2021-02-04 22:58:50 +0100

    init

---
+++ src/utilities.test.ts
@@ -0,0 +1,11 @@
+import { curry } from "./utilities";
+
+describe("curry", () => {
+  it("curries functions", () => {
+    const _add = (x, y) => x + y;
+    const add = curry(_add);
+    const add2 = add(2);
+
+    expect(add2(2)).toEqual(4);
+  });
+});
