378e288846b3ab4e7fb6f6d9f24b26a38634845a
Lev Kowalski <levkowalski@gmail.com>
2021-02-06 15:52:00 +0100

    wip

--- src/functor.test.ts
+++ src/functor.test.ts
@@ -2,6 +2,29 @@ import { Box } from "./functor";
 
 describe("Functors", () => {
   describe("Identity functor", () => {
+    describe("verify functor laws ", () => {
+      const f = (x: number) => x * 2;
+      const g = (x: number): Boolean => x % 2 === 0;
+      const h = (x) => g(f(x));
+      const identity = (x) => x;
+
+      test("composition law", () => {
+        const compaLawResult1 = Box(3).map(f).map(g);
+        const compaLawResult2 = Box(3).map(h);
+
+        expect(JSON.stringify(compaLawResult1)).toEqual(
+          JSON.stringify(compaLawResult2)
+        );
+      });
+
+      test("identity law result", () => {
+        const idResult1 = identity(Box(3));
+        const idResult2 = Box(3).map(identity);
+
+        expect(JSON.stringify(idResult1)).toEqual(JSON.stringify(idResult2));
+      });
+    });
+
     test("use case with no statefull variables", () => {
       const nextCharForNumberString = (str) =>
         Box(str)
